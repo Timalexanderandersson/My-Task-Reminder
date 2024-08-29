5845be6ac00b6b21d0c4f2ad84dd811932dd6aff
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-29 14:31:31 +0000

    created superuser and csrf_trusted_origins in settings

--- my_task_reminder/settings.py
+++ my_task_reminder/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k#89=i8x5swmhxeafaleu=*0!8too^99_yu-vv^3mqe1vrsrxc'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -91,6 +92,11 @@ DATABASES = {
     "default":
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

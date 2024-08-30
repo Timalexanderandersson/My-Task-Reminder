07828c50a6bfd8f46e06b0dc33dd3699d41026ee
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-30 09:23:31 +0000

    created models and admin for task_app

--- task_app/admin.py
+++ task_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Taskmodel, Task
 # Register your models here.
+
+@admin.register(Taskmodel)
+class Taskmodeladmin(admin.ModelAdmin):
+    list_display = ("user", "name")
+    
+
+@admin.register(Task)
+class Taskadmin(admin.ModelAdmin):
+    list_display = ("task_list", "description","completed","due_date")

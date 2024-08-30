b109c98836e2c9a60332af1deb56fd4ff6a63b83
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-30 12:09:57 +0000

    deleted models and admin code

--- task_app/admin.py
+++ task_app/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
-from .models import Taskmodel, Task
+
 # Register your models here.
 
-@admin.register(Taskmodel)
-class Taskmodeladmin(admin.ModelAdmin):
-    list_display = ("user", "name")
-    
-
-@admin.register(Task)
-class Taskadmin(admin.ModelAdmin):
-    list_display = ("task_list", "description","completed","due_date")
--- task_app/models.py
+++ task_app/models.py
@@ -1,23 +1,3 @@
-
 from django.db import models
-from django.contrib.auth.models import User
 
 # Task list models
-class Taskmodel(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, blank=True, related_name="tasklisting")
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-# Task model 
-class Task(models.Model):
-    task_list = models.ForeignKey(
-        Taskmodel, on_delete=models.CASCADE, related_name= "listoftask")
-    description = models.TextField(max_length=300)   
-    completed = models.BooleanField(default=False)
-    due_date = models.DateTimeField(blank=True, null=True)
-
-    def __str__(self):
-        return self.title
